097e76ba499bd6fc5e4ec09ee32f6f6ecb0cbe8e
51pwn MTX <hktalent@qq.com>
2022-01-12 22:06:03 +0800

    fix

--- servecmd.go
+++ servecmd.go
@@ -45,7 +45,7 @@ func sendReq(addressOfRequester net.Addr, domain1 string) {
 	//
 	req, err := http.NewRequest("POST", resUrl, post_body)
 	if err != nil {
-		req.Header.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Safari/605.1.15")
+		req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Safari/605.1.15")
 		req.Header.Add("Content-Type", "application/json;charset=UTF-8")
 		go http.DefaultClient.Do(req)
 		// req.Body.Close()
